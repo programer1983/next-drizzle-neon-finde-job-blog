c8b3a2905102bb14795b75912f4978037954f5b4
Prodi <programer972@gmail.com>
2026-04-24 16:38:36 +0300

    fix: simplify eslint config and ignore builds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,19 +17,31 @@
 
 // export default eslintConfig;
 
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+//   },
+// ];
+
+// export default eslintConfig;
+
+import { nextConfig } from "eslint-config-next/flat";
+
+/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextConfig,
   {
     ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
